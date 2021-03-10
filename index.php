458aafb89062cbaca93211bb31557e418cc07b3b
RicentCB <ricardo.cb009@gmail.com>
2021-03-10 12:32:20 -0600

    [ADD] Creacion de archivos necesarios

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>El arcoiris ADMIN</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Custom -->
+    <link rel="stylesheet" href="view/css/style.css">
+
+</head>
+<body >
+    <?php
+        include "modules/home.php";
+    ?>
+
+    <!-- SCRIPTS -->
+    <script src="view/js/template.js"></script>
+    <!-- /SCRIPTS -->
+    
+</body>
+</html>
