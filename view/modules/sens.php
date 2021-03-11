a68f7fdfb5bdcaef727a639e5076cec2ae727e4f
RicentCB <ricardo.cb009@gmail.com>
2021-03-10 23:15:58 -0600

    [ADD] Se toma en cuenta manejo de error

--- view/modules/sens.php
+++ view/modules/sens.php
@@ -1,6 +1,11 @@
 <?php
     require_once("controller/sensor.controller.php");
-    $valueTemp = $_GET["temp"];
-    $insert = SensorController::ctrInsert($valueTemp);
-    echo $insert;
+    if(isset($_GET["temp"])){
+        $valueTemp = $_GET["temp"];
+        $insert = SensorController::ctrInsert($valueTemp);
+        echo $insert;
+    }else{
+        //Variable no existe
+        echo "ERROR";
+    }
 ?>
