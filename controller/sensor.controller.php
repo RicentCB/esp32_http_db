8a6783a232fb8adb50ff71435f38fc780d493222
RicentCB <ricardo.cb009@gmail.com>
2021-03-10 22:11:27 -0600

    [ADD] Controlador para la pagina del sensor

--- controller/sensor.controller.php
+++ controller/sensor.controller.php
@@ -1,2 +1,16 @@
 <?php
-    
+    require_once ("model/sensor.model.php");
+
+    /*================================================
+        MODELO DE LA PAGINA SENSOR
+    ================================================*/
+    class SensorController{
+
+        //Insertar en tabla de Registro
+        static public function ctrInsert($value){
+            $value = (float) $value;
+            $data = array("value"=>$value);
+            $ans = SensorModel::mdlInsert($data);
+            echo $ans;
+        }
+    }
--- view/modules/sens.php
+++ view/modules/sens.php
@@ -2,7 +2,9 @@
     <h1>Sensor Temp</h1>
 </main>
 <?php
+    require_once("controller/sensor.controller.php");
     echo '<pre>';
     var_dump($_GET);
     echo '</pre>';
+    echo SensorController::ctrInsert("10000");
 ?>
