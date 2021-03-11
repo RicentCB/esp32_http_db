75b22c5eadfdc755d6bf62c1033302962b9f89c6
RicentCB <ricardo.cb009@gmail.com>
2021-03-10 22:11:06 -0600

    [ADD] Modelo para la pagina del sensor

--- model/sensor.model.php
+++ model/sensor.model.php
@@ -1,47 +1,35 @@
 <?php
-    require_once ("conexion.php");
+    require_once ("connection.php");
+    date_default_timezone_set('America/Mexico_City');
     /*================================================
         MODELO DE LA PAGINA SENSOR
     ================================================*/
-    class ClientsModel{
-        /*-----------------------------------------
-            C R E A T E   -   C L I E N T
-        -----------------------------------------*/
-        static public function mdlCreateClient($table, $data){
-            $sql = "INSERT INTO $table (`id_sucursal`, `nombre_Cliente`, `telefono_Cliente`, `RFC`, `email_Cliente`, `descuento`, `tieneCredito`, `direccion`, `colonia`, `codigoPostal`, `id_estadoMunicipio`) ";
-            $sql .="VALUES (:idPlace, :name, :phone, :RFC, :mail, :util, :credit, :adress, :neig, :ZIP, :local)";
+    class SensorModel{
+        //Insertar en tabla de Registros
+        static public function mdlInsert($data){
+            $sql  = "INSERT INTO `register_temp` (`val_valueTemp`, `timeReg_valueTemp`) ";
+            $sql .= "VALUES (:val, CURRENT_TIME)";
 
             $stmt = Conexion::connect()->prepare($sql);
-            $stmt -> bindParam(":idPlace", $data["idPlace"]);
-            $stmt -> bindParam(":name", $data["name"]);
-            $stmt -> bindParam(":phone", $data["phone"]);
-            $stmt -> bindParam(":RFC", $data["RFC"]);
-            $stmt -> bindParam(":mail", $data["mail"]);
-            $stmt -> bindParam(":util", $data["util"]);
-            $stmt -> bindParam(":credit", $data["credit"]);
-            $stmt -> bindParam(":adress", $data["adress"]);
-            $stmt -> bindParam(":neig", $data["neig"]);
-            $stmt -> bindParam(":ZIP", $data["ZIP"]);
-            $stmt -> bindParam(":local", $data["local"]);
+
+            $stmt -> bindParam(":val", $data["value"]);
 
             if($stmt->execute())
                 return true;
             else   
                 return false;
         }
-        /*-----------------------------------------
-            R E A D  -   C L I E N T
-        -----------------------------------------*/
-        static public function mdlShowClients($item, $value, $table="clientes"){
-            $sql  = "SELECT * FROM clientes clien ";
-            $sql .= "INNER JOIN estados_municipios em ON em.id = clien.id_estadoMunicipio ";
-            $sql .= "INNER JOIN municipios muni ON muni.id = em.municipios_id ";
+        //Leer tabla de Registros
+        static public function mdlRead($item=NULL, $value=NULL){
+            $sql  = "SELECT * FROM register_temp reg ";
+            
             if($item == null){
-                $sql .= "ORDER BY clien.nombre_Cliente ";
+                //Devolver todos los datos en la tabla
                 $stmt = Conexion::connect() -> prepare($sql);
                 $stmt -> execute();
                 return $stmt -> fetchALl();
             }else{
+                //Devolver fila especifica
                 $sql .= "WHERE $item LIKE :item ";
                 $stmt = Conexion::connect() -> prepare($sql);
                 $stmt -> bindParam(":item", $value);
@@ -49,34 +37,5 @@
                 return $stmt -> fetch();
             }
         }
-        /*-----------------------------------------
-            U P D A T E   -   C L I E N T
-        -----------------------------------------*/
-        static public function mdlUpdateClient($table, $data){
-            $sql  = "UPDATE $table SET nombre_Cliente = :name, telefono_Cliente = :phone, RFC = :RFC, ";
-            $sql .= "email_Cliente = :mail, descuento = :util, tieneCredito = :credit, direccion = :adress, ";
-            $sql .= " colonia = :neig, codigoPostal = :ZIP, id_estadoMunicipio = :local ";
-            $sql .= "WHERE ID_cliente LIKE :IDClient ";
 
-            $stmt = Conexion::connect()->prepare($sql);
-            $stmt -> bindParam(":name", $data["name"]);
-            $stmt -> bindParam(":phone", $data["phone"]);
-            $stmt -> bindParam(":RFC", $data["RFC"]);
-            $stmt -> bindParam(":mail", $data["mail"]);
-            $stmt -> bindParam(":util", $data["util"]);
-            $stmt -> bindParam(":credit", $data["credit"]);
-            $stmt -> bindParam(":adress", $data["adress"]);
-            $stmt -> bindParam(":neig", $data["neig"]);
-            $stmt -> bindParam(":ZIP", $data["ZIP"]);
-            $stmt -> bindParam(":local", $data["local"]);
-            //ID
-            $stmt -> bindParam(":IDClient", $data["IDClient"]);
-            
-
-            if($stmt->execute())
-                return true;
-            else   
-                return false;
-
-        }
     }
