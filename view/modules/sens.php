e66461256c07b06a4cb8e42af4d9391a963cea6f
RicentCB <ricardo.cb009@gmail.com>
2021-03-10 22:17:20 -0600

    [ADD] Script Funcionando con variables GET

--- view/modules/sens.php
+++ view/modules/sens.php
@@ -1,10 +1,6 @@
-<main>
-    <h1>Sensor Temp</h1>
-</main>
 <?php
     require_once("controller/sensor.controller.php");
-    echo '<pre>';
-    var_dump($_GET);
-    echo '</pre>';
-    echo SensorController::ctrInsert("10000");
+    $valueTemp = $_GET["temp"];
+    $insert = SensorController::ctrInsert($valueTemp);
+    echo $insert;
 ?>
